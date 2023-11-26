0dfdf9a11bd260398f29507a49bdf90e3640e1f2
Julia Bauer <juliabauer@ufl.edu>
2023-11-26 13:36:14 -0500

    add attractions

--- models/City.js
+++ models/City.js
@@ -21,8 +21,28 @@ const CitySchema = new Schema({
   tip: {
     type: String,
     required: true,
-  }
+  },
   
+  attraction1: {
+    type: String,
+  },
+
+  attraction2: {
+    type: String,
+  },
+
+  attraction3: {
+    type: String,
+  },
+
+  attraction4: {
+    type: String,
+  },
+
+  attraction5: {
+    type: String
+  }
+
 });
 
 module.exports = models.City || model("City", CitySchema);
